a8d71c079902d3d236546e75ad37648db04fc7df
Prajwal M <prajwalm1511@gmail.com>
2026-03-09 20:00:04 +0530

    Escape maze

---
+++ day_6.py
@@ -0,0 +1,20 @@
+#FUNCTIONS
+# PRINT , LEN, they are all built in functions
+# if we want to make our own functions
+#we use 'def'
+def my_function():  #"my_function" is the name of the function "() represents function" 
+    print("hello")
+    a=len("helloworld")
+    print(a)
+my_function()
+
+# for :
+# for item in list_of_items:
+#   do something in each item
+
+#while something_is_true:
+#   do something repeatedly
+a=5
+while a>0:
+    a -= 1
+    print(a)
